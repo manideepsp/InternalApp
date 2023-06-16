a3b45f94581133719bafb803a09f743d53ee75cd
Manideep <manideepsp.16@gmail.com>
2023-06-16 14:55:22 +0530

    re written the program class with objects in constructors constructors

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,67 +1,84 @@
 ﻿using BusinessModel;
 using System;
-using System.Collections.Generic;
 //using DataLayer;
 
 namespace ConsoleApp
 {
-	/// <summary>
-	/// program class
-	/// </summary>
-	public class Program
-	{
-		/// <summary>
-		/// main method
-		/// </summary>
-		public static void Main()
-		{
-			//objects creation
-			Authentication authenticate = new Authentication();
-			User user = new User();
+    /// <summary>
+    /// Represents the main program logic for user authentication.
+    /// </summary>
+    class Program
+    {
+        private Authentication auth;  // Object to handle authentication operations
+        private User user;  // Object representing user information
 
-			//DataSources.userData.Add(new List<string> { "name", "password", "emial", "mobile" });
-			//Console.WriteLine(DataSources.userData[0][1]);
+        /// <summary>
+        /// Constructor for the Program class.
+        /// Initializes the auth and user objects.
+        /// </summary>
+        public Program()
+        {
+            auth = new Authentication();
+            user = new User();
+        }
 
-			Console.WriteLine(Literal.div);
-			Console.WriteLine(Literal.menu);
-			int input = Convert.ToInt32(Console.ReadLine());
-			int redirect = input;
-			bool flag = true;
-			while (flag)
-			{
-				switch (redirect)
-				{
-					//exit
-					case 0:
-						flag = false;
-						break;
-					//login
-					case 1:
-						Console.WriteLine(Literal.div, Literal.login);
-						redirect = authenticate.Login(user);
-						break;
-					//register
-					case 2:
-						Console.WriteLine(Literal.div, Literal.register);
-						redirect = authenticate.Register(user);
-						break;
-					//forgot password
-					case 3:
-						Console.WriteLine(Literal.div, Literal.forgotPassword);
-						redirect = authenticate.ForgotPassword(user);
-						break;
-					//logout
-					case 4:
-						Console.WriteLine(Literal.div, Literal.loggedIn, Literal.logout);
-						redirect = authenticate.Logout();
-						break;
-					//default
-					default:
-						Console.WriteLine(Literal.div, Literal.switchDefault);
-						redirect = authenticate.SwitchDefault();
-						break;
-				}
-			}
-		}
-	}
+        /// <summary>
+        /// Runs the main program logic.
+        /// Displays a menu, takes user input, and performs the corresponding actions.
+        /// </summary>
+        public void Run()
+        {
+            Console.WriteLine(Literal.div);
+            Console.WriteLine(Literal.menu);
+            int input = Convert.ToInt32(Console.ReadLine());
+            int redirect = input;
+            bool flag = true;
+            while (flag)
+            {
+                switch (redirect)
+                {
+                    // Exit
+                    case 0:
+                        flag = false;
+                        break;
+                    // Login
+                    case 1:
+                        Console.WriteLine(Literal.div, Literal.login);
+                        redirect = auth.Login(user);
+                        break;
+                    // Register
+                    case 2:
+                        Console.WriteLine(Literal.div, Literal.register);
+                        redirect = auth.Register(user);
+                        break;
+                    // Forgot password
+                    case 3:
+                        Console.WriteLine(Literal.div, Literal.forgotPassword);
+                        redirect = auth.ForgotPassword(user);
+                        break;
+                    // Logout
+                    case 4:
+                        Console.WriteLine(Literal.div, Literal.loggedIn, Literal.logout);
+                        redirect = auth.Logout();
+                        break;
+                    // Default
+                    default:
+                        Console.WriteLine(Literal.div, Literal.switchDefault);
+                        redirect = auth.SwitchDefault();
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// The entry point of the program.
+        /// Creates an instance of the Program class and calls the Run method to start the program.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+    }
 }
